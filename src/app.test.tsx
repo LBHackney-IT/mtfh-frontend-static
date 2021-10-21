45906ed0b4077329b6d9e81b5a519c80c13f2d2a
srcrustaceo <ureciomunicio@deloitte.co.uk>
2021-10-21 16:48:04 +0200

    update(deps): locks versions

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -1,6 +1,8 @@
 import React from "react";
+
 import { render } from "@hackney/mtfh-test-utils";
 import { screen } from "@testing-library/react";
+
 import App from "./app";
 
 describe("<App />", () => {
--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
-import { Route, Switch } from "react-router-dom";
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 
 import { StaticView } from "./views/static-view";
 
